66e18b79602d915b9e6133d907e90c3d46e7cff4
Veronicandemo <ndemoveronica@gmial.com>
2024-08-03 13:43:27 +0300

    initial commit: add the inital classes

---
+++ src/_3d_modeller/viewer.py
@@ -0,0 +1,115 @@
+class Viewer(object):
+    def __init__(self):
+        """Initialize the viewer"""
+        self.init_interface()
+        self.init_opengl()
+        self.init_scene()
+        self.init_interaction()
+        init_primitives()
+
+    def init_interface(self):
+        """ Initialize the window and register the render function """
+        glutInit()
+        glutInitWindowSize(640, 480)
+        glutCreateWindow("3D Modeller")
+        glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+        glutDisplayFunc(self.render)
+
+    def init_opengl(self):
+        """ initialize the opengl settings to render the scene """
+        self.inverseModelView() = numpy.identity(4)
+        self.modelView() = numpy.identity(4)
+
+        glEnable(GL_CULL_FACE)
+        glCulFace(GL_BLACK)
+        glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LESS)
+
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 0, 1, 0))
+        glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, 0, -1))
+
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glEnable(GL_COLOR_MATERIAL)
+        glClearColor(0.4, 0.4, 0.4, 0.0)
+
+    def init_scene(self):
+        """ initialize the scene object and initial scene """
+        self.scene = Scene()
+        self.create_sample_scene()
+
+    def create_sample_scene(self):
+        cube_node = Cube()
+        cube_node.translate(2, 0, 2)
+        cube_node.color_index = 2
+        self.scene.add_node(cube_node)
+
+        sphere_node = Sphere()
+        sphere_node.translate(-2, 0, 2)
+        sphere_node.color_index = 3
+        self.scene.add_node(sphere_node)
+
+        hierarchical_node = SnowFigure()
+        hierarchical_node.translate(-2, 0, -2)
+        self.scene.add_node(hierarchical_node)
+
+    def init_interaction(self):
+        """ init user interaction and callbacks """
+        self.interaction = Interaction()
+        self.interaction.register_callback('pick', self.pick)
+        self.interaction.register_callback('move', self.move)
+        self.interaction.register_callback('place', self.place)
+        self.interaction.register_callback('rotate_color', self.rotate_color)
+        self.interaction.register_callback('scale', self.scale)
+
+    def main_loop(self):
+        glutMainLoop()
+
+    if __name__ == "__main__":
+        viewer = Viewer()
+        viewer.main_loop()
+
+    def render(self):
+        """ The render pass for the scene """
+        self.init_view()
+        glEnable(GL_LIGHTING)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Load the modelview matrix from the current state of the trackball
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        glLoadIdentity()
+        loc = self.interaction.translation
+        glTranslated(loc[0], loc[1], loc[2])
+        glMultMatrixf(self.interaction.trackball.matrix)
+
+        # store the inverse of the current modelview.
+        currentModelView = numpy.array(glGetFloatv(GL_MODELVIEW_MATRIX))
+        self.modelView = numpy.transpose(currentModelView)
+        self.inverseModelView = inv(numpy.transpose(currentModelView))
+
+        # render the scene. This will call the render function for each object
+        # in the scene
+        self.scene.render()
+
+        # draw the grid
+        glDisable(GL_LIGHTING)
+        glCallList(G_OBJ_PLANE)
+        glPopMatrix()
+
+        # flush the buffers so that the scene can be drawn
+        glFlush()
+
+    def init_view(self):
+        """ initialize the projection matrix """
+        xSize, ySize = glutGet(GLUT_WINDOW_WIDTH), glutGet(GLUT_WINDOW_HEIGHT)
+        aspect_ratio = float(xSize) / float(ySize)
+
+        # load the projection matrix. Always the same
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+        glViewport(0, 0, xSize, ySize)
+        gluPerspective(70, aspect_ratio, 0.1, 1000.0)
+        glTranslated(0, 0, -15)
+
